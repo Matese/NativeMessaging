780dc7ad8d96840ff5c3fe406c17d563fb9b26b0
Fábio Augusto Pandolfo <fabio@digitaldoc.com.br>
2022-02-24 15:27:24 -0300

    cleanup

--- examples/native-messaging/add-on/background.js
+++ examples/native-messaging/add-on/background.js
@@ -16,15 +16,4 @@ On a click on the browser action, send the app a message.
 browser.browserAction.onClicked.addListener(() => {
   console.log("Sending: ping");
   port.postMessage("ping");
-  
-  // const result = await browser.runtime
-  //   .sendNativeMessage("ping_pong", "ping")
-  //   .then(e => { 
-  //     return e 
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
-
-  // console.log(result);
 });
